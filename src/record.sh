6a7c3b0b65ef4e53dbc36a6c6a86c21643c16ccd
maaraa <maaraa@dotworld.in>
2018-04-04 17:44:19 +1000

    feature: Notify user on command details while executing

--- src/record.sh
+++ src/record.sh
@@ -125,7 +125,7 @@ function aboveAsOption() {
         getRecordFilename
         PREV_CMD=$(history 2 | head -n1 | cut -c 8-)
         echo ${EEXT} "\nAdded => ${PREV_CMD}\n"
-        echo "=======================> Executing ${PREV_CMD}"
+        echo "=======================> Executing ${PREV_CMD}" >> ${FN}
         echo "${PREV_CMD}" >> ${FN}
         return
     else
